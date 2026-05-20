141317f04c928af0a01b643989b0dbab4b551a34
Tran Anh Cuong <trananhcuong683@gmail.com>
2026-05-20 11:14:27 +0700

    styling out the form

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,25 @@
+import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { ClientInfo } from '../models/user.model';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('Angular-form');
+
+  formStep = 1;
+
+  clientInfo = new ClientInfo('', '', '')
+
+  onSubmit(form: NgForm) {
+    console.log('Your Info', {
+      client: this.clientInfo
+    })
+  }
 }
